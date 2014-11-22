372c77f36c13c8ece63a6a0ae2848b783044b6a8
Dustin Campbell <dustin@teamcampbell.org>
2014-11-22 07:44:42 -0800

    Add initial Memory class with default page size of 4K

---
+++ Source/Core/Emulation.Core.Tests/MemoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Emulation.Core.Tests
+{
+    public class MemoryTests
+    {
+        [Fact(DisplayName ="Default page size is 4096")]
+        public void DefaultPageSize()
+        {
+            var memory = new Memory();
+            Assert.Equal(4096, memory.PageSize);
+        }
+    }
+}
---
+++ Source/Core/Emulation.Core/Memory.cs
@@ -0,0 +1,14 @@
+﻿namespace Emulation.Core
+{
+    public class Memory
+    {
+        private readonly int pageSize;
+
+        public Memory(int pageSize = 4096)
+        {
+            this.pageSize = pageSize;
+        }
+
+        public int PageSize => pageSize;
+    }
+}
