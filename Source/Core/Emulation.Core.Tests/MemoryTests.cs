498c9bcf719b38bab8ea05099773ffe17c101fb0
Dustin Campbell <dustin@teamcampbell.org>
2014-11-22 08:14:04 -0800

    Support reading and writing of bytes

--- Source/Core/Emulation.Core.Tests/MemoryTests.cs
+++ Source/Core/Emulation.Core.Tests/MemoryTests.cs
@@ -4,11 +4,66 @@ namespace Emulation.Core.Tests
 {
     public class MemoryTests
     {
-        [Fact(DisplayName ="Default page size is 4096")]
+        [Fact(DisplayName = "Default page size is 4096")]
         public void DefaultPageSize()
         {
-            var memory = new Memory();
+            var memory = Memory.Create();
             Assert.Equal(4096, memory.PageSize);
+        }
+
+        [Fact(DisplayName = "Default size is 4096")]
+        public void DefaultSize()
+        {
+            var memory = Memory.Create();
+            Assert.Equal(4096, memory.Size);
+        }
+
+        [Fact(DisplayName = "Write and read bytes into default memory using indexer")]
+        public void WriteAndReadBytes_Indexer()
+        {
+            var memory = Memory.Create();
+
+            for (int i = 0; i < memory.Size; i++)
+            {
+                memory[i] = (byte)(i % 256);
+            }
+
+            for (int i = 0; i < memory.Size; i++)
+            {
+                Assert.Equal((byte)(i % 256), memory[i]);
+            }
+        }
+
+        [Fact(DisplayName = "Write and read bytes into 64k memory using indexer")]
+        public void WriteAndReadBytes_64k_Indexer()
+        {
+            var memory = Memory.Create(size: 64 * 1024);
+
+            for (int i = 0; i < memory.Size; i++)
+            {
+                memory[i] = (byte)(i % 256);
+            }
+
+            for (int i = 0; i < memory.Size; i++)
+            {
+                Assert.Equal((byte)(i % 256), memory[i]);
+            }
+        }
+
+        [Fact(DisplayName = "Write and read bytes into 64k memory")]
+        public void WriteAndReadBytes_64k()
+        {
+            var memory = Memory.Create(size: 64 * 1024);
+
+            for (int i = 0; i < memory.Size; i++)
+            {
+                memory.WriteByte(i, (byte)(i % 256));
+            }
+
+            for (int i = 0; i < memory.Size; i++)
+            {
+                Assert.Equal((byte)(i % 256), memory.ReadByte(i));
+            }
         }
     }
 }
--- Source/Core/Emulation.Core/Memory.cs
+++ Source/Core/Emulation.Core/Memory.cs
@@ -1,14 +1,94 @@
-﻿namespace Emulation.Core
+﻿using System;
+using System.Diagnostics;
+
+namespace Emulation.Core
 {
     public class Memory
     {
+        private readonly int size;
         private readonly int pageSize;
+        private readonly int pageCount;
 
-        public Memory(int pageSize = 4096)
+        private readonly byte[][] pages;
+
+        public static Memory Create(int size = 4096, int pageSize = 4096)
         {
-            this.pageSize = pageSize;
+            return new Memory(size, pageSize);
         }
 
+        private Memory(int size, int pageSize)
+        {
+            this.size = size;
+            this.pageSize = pageSize;
+
+            this.pageCount = size / pageSize;
+            if (size % pageSize > 0)
+            {
+                this.pageCount += 1;
+            }
+
+            this.pages = new byte[this.pageCount][];
+        }
+
+        private void EnsurePage(int page)
+        {
+            if (this.pages[page] == null)
+            {
+                this.pages[page] = new byte[this.pageSize];
+            }
+        }
+
+        private byte ReadByteCore(int address)
+        {
+            Debug.Assert(address >= 0 && address < this.size);
+
+            var page = address / this.pageSize;
+            var index = address % this.pageSize;
+
+            EnsurePage(page);
+
+            return this.pages[page][index];
+        }
+
+        private void WriteByteCore(int address, byte value)
+        {
+            Debug.Assert(address >= 0 && address < this.size);
+
+            var page = address / this.pageSize;
+            var index = address % this.pageSize;
+
+            EnsurePage(page);
+
+            this.pages[page][index] = value;
+        }
+
+        public byte ReadByte(int address)
+        {
+            if (address < 0 || address >= this.size)
+            {
+                throw new ArgumentOutOfRangeException("address");
+            }
+
+            return ReadByteCore(address);
+        }
+
+        public void WriteByte(int address, byte value)
+        {
+            if (address < 0 || address >= this.size)
+            {
+                throw new ArgumentOutOfRangeException("address");
+            }
+
+            WriteByteCore(address, value);
+        }
+
+        public int Size => size;
         public int PageSize => pageSize;
+
+        public byte this[int address]
+        {
+            get { return ReadByte(address); }
+            set { WriteByte(address, value); }
+        }
     }
 }
