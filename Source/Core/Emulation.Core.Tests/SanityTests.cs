db48e4ce78d6436180e7c3056787662b55285868
Dustin Campbell <dustin@teamcampbell.org>
2014-11-22 06:33:09 -0800

    Add xUnit support and simple sanity test

---
+++ Source/Core/Emulation.Core.Tests/SanityTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Emulation.Core.Tests
+{
+    public class SanityTests
+    {
+        [Fact]
+        public void XunitIsWorking()
+        {
+            Assert.True(true);
+            Assert.False(false);
+        }
+    }
+}
