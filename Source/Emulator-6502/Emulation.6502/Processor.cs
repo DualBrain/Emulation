ad0959c1792245afb3ee4aa6c7811f1a2630cac1
Dustin Campbell <dustin@teamcampbell.org>
2014-11-26 09:44:42 -0800

    Add Processor class

---
+++ Source/Emulator-6502/Emulation.6502/Processor.cs
@@ -0,0 +1,16 @@
+﻿using Emulation.Core;
+
+namespace Emulation_6502
+{
+    public class Processor
+    {
+        private readonly Memory memory;
+
+        public Processor()
+        {
+            this.memory = Memory.Create(64 * 1024);
+        }
+
+        public Memory Memory => this.memory;
+    }
+}
