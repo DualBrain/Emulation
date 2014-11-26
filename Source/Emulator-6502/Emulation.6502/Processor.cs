299869e383374caa6f6b84a090da6d33643198cf
Dustin Campbell <dustin@teamcampbell.org>
2014-11-26 09:58:46 -0800

    Add register and flag fields

--- Source/Emulator-6502/Emulation.6502/Processor.cs
+++ Source/Emulator-6502/Emulation.6502/Processor.cs
@@ -6,11 +6,26 @@ namespace Emulation_6502
     {
         private readonly Memory memory;
 
+        // registers
+        private ushort pc;
+        private byte sp;
+        private byte accumulator;
+        private byte x;
+        private byte y;
+        private byte status;
+
         public Processor()
         {
             this.memory = Memory.Create(64 * 1024);
         }
 
         public Memory Memory => this.memory;
+
+        public ushort PC => this.pc;
+        public byte SP => this.sp;
+        public byte Accumulator => this.accumulator;
+        public byte X => this.x;
+        public byte Y => this.y;
+        public byte Status => this.status;
     }
 }
