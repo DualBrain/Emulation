f30201d86dd62bfb96bc9623149be48e28af7cc9
Dustin Campbell <dustin@teamcampbell.org>
2014-12-04 17:38:07 -0800

    Display characters in memory view

--- Source/Debugger/Emulation.Debugger/ViewModels/MemoryLineViewModel.cs
+++ Source/Debugger/Emulation.Debugger/ViewModels/MemoryLineViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Emulation.Core;
+﻿using Emulation.Core;
 using Emulation.Debugger.MVVM;
 
 namespace Emulation.Debugger.ViewModels
@@ -22,6 +21,22 @@ namespace Emulation.Debugger.ViewModels
                 ? this.memory.ReadByte(address)
                 : (byte?)null;
 
+        private char? GetChar(int address)
+        {
+            var value = GetValue(address);
+            if (value == null)
+            {
+                return null;
+            }
+
+            var ch = (char)value.Value;
+            return char.IsControl(ch)
+                ? '.'
+                : ch == (char)0xad // soft hyphens are ignore in WPF
+                    ? '-'
+                    : ch;
+        }
+
         public int Address => this.address;
         public string AddressText => address.ToString("x" + this.hexWidth.ToString());
 
@@ -41,5 +56,22 @@ namespace Emulation.Debugger.ViewModels
         public byte? Byte14 => GetValue(this.address + 13);
         public byte? Byte15 => GetValue(this.address + 14);
         public byte? Byte16 => GetValue(this.address + 15);
+
+        public char? Char1 => GetChar(this.address);
+        public char? Char2 => GetChar(this.address + 1);
+        public char? Char3 => GetChar(this.address + 2);
+        public char? Char4 => GetChar(this.address + 3);
+        public char? Char5 => GetChar(this.address + 4);
+        public char? Char6 => GetChar(this.address + 5);
+        public char? Char7 => GetChar(this.address + 6);
+        public char? Char8 => GetChar(this.address + 7);
+        public char? Char9 => GetChar(this.address + 8);
+        public char? Char10 => GetChar(this.address + 9);
+        public char? Char11 => GetChar(this.address + 10);
+        public char? Char12 => GetChar(this.address + 11);
+        public char? Char13 => GetChar(this.address + 12);
+        public char? Char14 => GetChar(this.address + 13);
+        public char? Char15 => GetChar(this.address + 14);
+        public char? Char16 => GetChar(this.address + 15);
     }
 }
